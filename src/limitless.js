e7df53a01824335597635f6e5b2abd2b886df656
TomLiang <632585468@qq.com>
2025-11-22 23:52:09 +0800

    fixed领取问题

--- src/limitless.js
+++ src/limitless.js
@@ -243,6 +243,7 @@ class LimitlessSDK {
     const positions = await this._getPositions();
     const claims = (positions?.clob || []).filter(e =>
       e.market.closed &&
+      !e.market.winningOutcomeIndex &&
       (Number(e.positions.yes.unrealizedPnl) > 1000 || Number(e.positions.no.unrealizedPnl) > 1000));
     const conditional = new ethers.Contract(CONDITIONALTOKENS_ADDRESS, CONDITIONALTOKENS_ABI, this.wallet);
 
